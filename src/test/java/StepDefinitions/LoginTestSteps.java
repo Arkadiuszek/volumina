9210c30838ef2c6c11ab227981c7fc888032ed0e
Arkadiuszek <arek.siwak@gmail.com>
2021-02-19 12:42:09 +0100

    minor fix

--- src/test/java/StepDefinitions/LoginTestSteps.java
+++ src/test/java/StepDefinitions/LoginTestSteps.java
@@ -7,7 +7,6 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
